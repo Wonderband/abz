829b0772f5dac6dfb5abae18b51023d9a1d56aca
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-05-25 13:38:21 +0300

    Update UserForm.jsx

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,25 +1,19 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { Button } from "../Button/Button";
 import css from "./UserForm.module.scss";
 
 export const UserForm = () => {
-  const fileInputRef = useRef(null);
-  const [photo, setPhoto] = useState("");
+  const [photo, setPhoto] = useState("Upload your photo");
 
-  // const handleButtonClick = () => {
-  //   fileInputRef.current.click();
-  // };
+  const handleSubmit = () => {};
 
   const handleFileChange = (event) => {
     setPhoto(event.target.files[0].name);
-
-    console.log(photo);
-    // Do something with the selected file
   };
 
   return (
     <section>
-      <h2>Working with POST request</h2>
       <Formik
         initialValues={
           {
@@ -62,93 +56,16 @@ export const UserForm = () => {
               </label>
               {/* <div>Picked: {values.picked}</div> */}
             </div>
-            <label
-              htmlFor="file"
-              // onClick={handleButtonClick}
-              // onClick={document.getElementById("file").click()}
-            >
-              Upload
-            </label>
+            <label htmlFor="file">Upload</label>
             <input
               id="file"
               name="file"
               type="file"
-              ref={fileInputRef}
               onChange={handleFileChange}
               style={{ display: "none" }}
-              // onChange={(event) => {
-              //   setFieldValue("file", event.currentTarget.files[0]);
-              // }}
-              // className="form-control"
             />
             <div>{photo}</div>
-            {/* <input
-              type="button"
-              id="loadFileXml"
-              value="loadXml"
-              onclick="document.getElementById('file').click();"
-            /> */}
-            {/* <Thumb file={values.file} /> */}
-
-            <label htmlFor="firstName">Photo</label>
-            <Field id="firstName" name="firstName" placeholder="" />
-
-            <div className={css.inputs}>
-              {values.type === "EXPENSE" && (
-                <label className={css.selector}>
-                  {
-                    <Field
-                      name="categoryId"
-                      // component={SelectField}
-                      // options={newCategoriesList}
-                      required
-                    />
-                  }
-                </label>
-              )}
-              <ErrorMessage name="categoryId" />
-              <div className={css.amountDate}>
-                <label>
-                  <Field
-                    type="number"
-                    name="amount"
-                    step="0.01"
-                    placeholder="0.00"
-                    className={css.selectOption}
-                  />
-                  <ErrorMessage
-                    render={(msg) => (
-                      <div className={css.errorValidation}>{msg}</div>
-                    )}
-                    name="amount"
-                  />
-                </label>
-
-                <label>
-                  <Field
-                    className={css.selectOption}
-                    type="date"
-                    name="transactionDate"
-                    value={values.transactionDate}
-                  />
-                </label>
-                <ErrorMessage
-                  render={(msg) => (
-                    <div className={css.errorValidation}>{msg}</div>
-                  )}
-                  name="transactionDate"
-                />
-              </div>
-
-              <label>
-                <Field
-                  as="textarea"
-                  name="comment"
-                  placeholder="Comment"
-                  className={css.commentArea}
-                />
-              </label>
-            </div>
+            <Button label="Sign up" onClick={handleSubmit} />
           </Form>
         )}
       </Formik>
