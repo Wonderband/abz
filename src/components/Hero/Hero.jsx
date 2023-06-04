6bff5d82ecd5f2c3f8799f0f699f80addeea838d
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-06-04 15:48:02 +0300

    responsive done 2 hours

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,15 +11,17 @@ export const Hero = ({ onResetForm }) => {
   };
   return (
     <section className={css.heroSection}>
-      <h1 className={css.title}>Test assignment for front-end developer</h1>
-      <p className={css.descripton}>
-        What defines a good front-end developer is one that has skilled
-        knowledge of HTML, CSS, JS with a vast understanding of User design
-        thinking as they'll be building web interfaces with accessibility in
-        mind. They should also be excited to learn, as the world of Front-End
-        Development keeps evolving.
-      </p>
-      <Button label="Sign up" clickHandler={resetUserForm} />
+      <div className={css.textContainer}>
+        <h1 className={css.title}>Test assignment for front-end developer</h1>
+        <p className={css.descripton}>
+          What defines a good front-end developer is one that has skilled
+          knowledge of HTML, CSS, JS with a vast understanding of User design
+          thinking as they'll be building web interfaces with accessibility in
+          mind. They should also be excited to learn, as the world of Front-End
+          Development keeps evolving.
+        </p>
+        <Button label="Sign up" clickHandler={resetUserForm} />
+      </div>
     </section>
   );
 };
